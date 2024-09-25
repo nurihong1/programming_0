6a991f2efce69ed042c920360e981441675cce18
nurihong1 <nulihong6@gmail.com>
2024-09-25 14:14:31 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -3,7 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	printf("Hello, Sookmyung Electronics!|n");
+int main(void) {
+	int input_int;
+	float input_float;
+	
+	printf("enter an integer : ");
+	scanf("%d", &input_int);
+	
+	printf("enter a float");
+	scanf("%f", &input_float);
+	
+	printf("integer : %d, float : %f\n", input_int, input_float);
+	
 	return 0;
 }
