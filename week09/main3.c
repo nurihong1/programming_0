b4217ad52dc1c388046807561dd5e478bbb09c54
nurihong1 <nulihong6@gmail.com>
2024-10-30 14:21:28 +0900

    week09

---
+++ week09/main3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void f(void);
+
+int i;
+int main(void) {
+	
+	for (i=0;i<5;i++)
+	{
+		f();
+	}
+	return 0;
+}
+
+void f(void)
+{
+	for (i=0;i<10;i++)
+	printf("#");
+}
+
+/* f함수에서 나왔을 때 i=9 */ 
