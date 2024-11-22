ca41fcf312ea4c62bf0beb2093937b1054fa8607
nurihong1 <nulihong6@gmail.com>
2024-11-22 15:37:29 +0900

    week12

---
+++ week12/main4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) 
+{
+	int i;
+	int grade[5];
+	
+	for (i=0;i<5;i++)
+	{
+		printf("Input value - grade[%i] = ",i);
+		scanf("%d", &grade[i]);
+	}
+	int *ptr=grade;
+	int sum=0;
+	for (i=0;i<5;i++)
+	{
+		printf("grade[%i] = %i\n", i,*(ptr+i));
+		sum+=*(ptr+i);
+	}
+	printf("average: %i", sum/5);
+	return 0;
+}
